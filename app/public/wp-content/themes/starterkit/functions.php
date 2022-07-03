310a4c32ca11ec538010301bae1341ef8eb8664f
Leesa Ward <leesa@doubleedesign.com.au>
2022-07-03 21:43:54 +1000

    SCSS linting and such

--- app/public/wp-content/themes/starterkit/functions.php
+++ app/public/wp-content/themes/starterkit/functions.php
@@ -2,14 +2,14 @@
 /**
  * starterkit functions and definitions
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package starterkit
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if( ! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -26,10 +26,10 @@ function starterkit_setup() {
 		* If you're building a theme based on starterkit, use a find and replace
 		* to change 'starterkit' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'starterkit', get_template_directory() . '/languages' );
+	load_theme_textdomain('starterkit', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +37,19 @@ function starterkit_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'starterkit' ),
+			'header' => esc_html__('Header', 'starterkit'),
 		)
 	);
 
@@ -71,7 +71,7 @@ function starterkit_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -88,7 +88,7 @@ function starterkit_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'starterkit_setup' );
+add_action('after_setup_theme', 'starterkit_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -98,24 +98,24 @@ add_action( 'after_setup_theme', 'starterkit_setup' );
  * @global int $content_width
  */
 function starterkit_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'starterkit_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters('starterkit_content_width', 640);
 }
-add_action( 'after_setup_theme', 'starterkit_content_width', 0 );
+add_action('after_setup_theme', 'starterkit_content_width', 0);
 
 /**
  * Enqueue scripts and styles.
  */
 function starterkit_scripts() {
-	wp_enqueue_style( 'starterkit-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'starterkit-style', 'rtl', 'replace' );
+	wp_enqueue_style('starterkit-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('starterkit-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'starterkit-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('starterkit-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if(is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'starterkit_scripts' );
+add_action('wp_enqueue_scripts', 'starterkit_scripts');
 
 /**
  * Custom template tags for this theme.
@@ -135,7 +135,7 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if(defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
