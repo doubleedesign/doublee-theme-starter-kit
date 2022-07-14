c14769c1c63fb31baa23d742b75ab5dfa55372d5
Leesa Ward <leesa@doubleedesign.com.au>
2022-07-14 23:31:55 +1000

    Formatting, linting, and PHPStorm config stuff

--- app/public/wp-content/themes/starterkit/gulpfile.js
+++ app/public/wp-content/themes/starterkit/gulpfile.js
@@ -18,17 +18,21 @@ gulp.task('styles', (done) => {
 	gulp.src('scss/style.scss')
 		.pipe(sassGlob())
 		.pipe(sourcemaps.init())
-		.pipe(plumber(function(error) {
-			// eslint-disable-next-line no-console
-			console.log(error);
-			this.emit('end');
-		}))
+		.pipe(
+			plumber(function(error) {
+				// eslint-disable-next-line no-console
+				console.log(error);
+				this.emit('end');
+			})
+		)
 		.pipe(sass())
 		.pipe(postCSS([objectFitImages]))
-		.pipe(autoprefixer({
-			browsers: ['defaults', 'iOS >= 8'],
-			grid: false,
-		}))
+		.pipe(
+			autoprefixer({
+				browsers: ['defaults', 'iOS >= 8'],
+				grid: false,
+			})
+		)
 		.pipe(minifyCSS())
 		.pipe(rename('style.css'))
 		.pipe(sourcemaps.write('/', ''))
@@ -43,10 +47,12 @@ gulp.task('editor-styles', (done) => {
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(postCSS([objectFitImages]))
-		.pipe(autoprefixer({
-			browsers: ['defaults', 'iOS >= 8'],
-			grid: false,
-		}))
+		.pipe(
+			autoprefixer({
+				browsers: ['defaults', 'iOS >= 8'],
+				grid: false,
+			})
+		)
 		.pipe(minifyCSS())
 		.pipe(rename('editor-styles.css'))
 		.pipe(sourcemaps.write('/'))
@@ -58,18 +64,20 @@ gulp.task('editor-styles', (done) => {
 gulp.task('scripts', function(done) {
 	gulp.src('./js/**/*.js')
 		.pipe(sourcemaps.init())
-		.pipe(gru2.rollup({
-			input: 'js/theme.js',
-			external: ['window'],
-			cache: true,
-			output: [
-				{
-					file: 'theme.bundle.js',
-					format: 'es',
-					globals: { window: 'window' },
-				},
-			],
-		}))
+		.pipe(
+			gru2.rollup({
+				input: 'js/theme.js',
+				external: ['window'],
+				cache: true,
+				output: [
+					{
+						file: 'theme.bundle.js',
+						format: 'es',
+						globals: { window: 'window' },
+					},
+				],
+			})
+		)
 		.pipe(sourcemaps.write('/', ''))
 		.pipe(gulp.dest('./js/dist'));
 	done();
@@ -78,18 +86,20 @@ gulp.task('scripts', function(done) {
 gulp.task('vendor', (done) => {
 	gulp.src('./js/vendor/**/*.js')
 		.pipe(concat('vendor.js'))
-		.pipe(gru2.rollup({
-			input: './js/vendor.js',
-			external: ['window'],
-			cache: true,
-			output: [
-				{
-					file: 'vendor.bundle.js',
-					format: 'es',
-					globals: { window: 'window' },
-				},
-			],
-		}))
+		.pipe(
+			gru2.rollup({
+				input: './js/vendor.js',
+				external: ['window'],
+				cache: true,
+				output: [
+					{
+						file: 'vendor.bundle.js',
+						format: 'es',
+						globals: { window: 'window' },
+					},
+				],
+			})
+		)
 		.pipe(gulp.dest('js/dist'))
 		.pipe(browsersync.stream());
 	done();
@@ -107,8 +117,10 @@ gulp.task('build', function() {
 
 	gulp.watch('scss/**/*.scss', gulp.series('styles'));
 	gulp.watch('scss/**/*.scss', gulp.series('editor-styles'));
-	gulp.watch('js/[^_]*.js', gulp.series('scripts'));
+	gulp.watch('js/theme/*.js', gulp.series('scripts'));
+	gulp.watch('js/theme.js', gulp.series('scripts'));
 	gulp.watch('js/vendor/[^_]*.js', gulp.series('vendor'));
+
 	browsersync.reload();
 });
 
